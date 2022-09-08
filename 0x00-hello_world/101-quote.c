2df9310a72046e5b545a646020dbb95c8c917bf6
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-08 13:48:20 -0700

    Fix : Standard errors in C with return 1:

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Entry pointi
  * Return: 1 (Error)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
