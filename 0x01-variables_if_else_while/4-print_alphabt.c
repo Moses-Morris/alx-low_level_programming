4a11421d3a85947a46ddb70d7a3e716ffd8c2462
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-09 06:53:36 -0700

    Print all alphabets except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+		}
+		else
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
