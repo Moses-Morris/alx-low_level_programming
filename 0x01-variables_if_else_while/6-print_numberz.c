c1b802871ef0152beb3e10fe6d0edde18715c15e
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-09 07:48:25 -0700

    Print numbers as characters> You are not allowed to use any variable of type char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* Authored By:  Moses Morris Njuguna */
+
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char y;
+
+	for (y = '0'; y <= '9'; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
