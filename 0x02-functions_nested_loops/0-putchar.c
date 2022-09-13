f934ebc15d1e421085679c9e37057e1d8564a02b
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-13 02:35:51 -0700

    The update on Betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,21 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <main.h>
 
 /* Authored By:  Moses Morris Njuguna */
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Entry point
+ * Description: 'Print out the value _putchar'
+ * _putchar - writes the character c to stdout.
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  * The program does not use printf or put function.
- * The write function tells: 1-is the file descriptor being told to give standard output
- * 9 - is the number of characters that will be written by as bytes of the array.
+ * The write function tells:
+ * 1-is the file descriptor being told to give standard output
+ * 9 - is the number of characters to be written as bytes of the array.
  */
 
 int main(void)
 {
-	write(1,"_putchar\n",9);
+	write(1, "_putchar\n", 9);
 	return (0);
 }
