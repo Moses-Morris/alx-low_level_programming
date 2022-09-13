745463dec9b623c3e622e517a5d4f000453ff4ef
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-13 03:03:10 -0700

    A function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,12 @@
-/** The header file
- * */
+/*The header file */
+/*This file will contain all our prototype functions*/
+/*It will be included as a header file*/
+void print_alphabet(void)
+{
+	int a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
