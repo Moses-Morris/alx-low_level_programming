e6ea2c814724ec1f027663de07ceb776aa81f137
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-13 08:11:00 -0700

    Fix : a function that prints the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,10 @@
 #include "main.h"
 /* Authored By:  Moses Morris Njuguna */
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  * print_alphabet - Prints the alphabet in lowercase
  * _putchar - writes the character c to stdout.
- * Description: 'Print out the value _putchar'
+ * Description: 'print_alphabet'
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  * The program does not use printf or put function.
