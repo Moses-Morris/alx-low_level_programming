103f15e3f056a2d2e0df207f1aa26051627dfae5
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-13 11:27:38 -0700

    Before moving

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -16,19 +16,18 @@
  */
 int _islower(int c)
 {
-	int lower;
-
-	lower = islower(c);
-	if (lower > 0)
-	{
-		int t = 1;
-
-		_putchar(t);
-	} else
+	int r = islower(c);
+	if (r == 0)
 	{
 		int z = 0;
 
 		_putchar(z);
+		return (0);
+	} else
+	{
+		int t = 1;
+
+		_putchar(t);
+		return (0);
 	}
-	return (0);
 }
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include <stdio.h>
+#include "1-alphabet.c"
+#include "2-print_alphabet_x10.c"
+#include "3-islower.c"
+#include "_putchar.c"
 
 /**
  * main - check the code
@@ -8,5 +13,16 @@
 int main(void)
 {
     print_alphabet();
+
+    int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    
     return (0);
 }
