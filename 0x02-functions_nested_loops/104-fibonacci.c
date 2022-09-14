539abb979605bb5e8918f4ca0ab93a884b7f81f9
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-14 02:39:56 -0700

    Find and print the first 98 fib numbers starting with 1 and 2.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,7 +14,7 @@
  * The numbers should be separated by comma, followed by a space ,
  * You are allowed to use the standard library
  * You are not allowed to use any other library (You can’t use GMP etc…)
- * You are not allowed to use long long, malloc, pointers, arrays/tables, or structures
+ * not to use long long, malloc, pointers, arrays/tables, or structures
  * You are not allowed to hard code any Fibonacci number (except for 1 and 2)
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
