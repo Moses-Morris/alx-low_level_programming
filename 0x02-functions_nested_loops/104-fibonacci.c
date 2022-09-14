622f7dd25db3961239407a60d57bc14d1c94268b
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-14 02:37:39 -0700

    Find and print the first 98 fib numbers starting with 1 and 2.

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+#define LARGEST 10000000000
+
+/*Authored by Moses Morris*/
+
+/*Betty Lint Style*/
+
+/**
+ * main - Entry point.
+ * _putchar - writes the character c to stdout.
+ * Description: 'Fibanoci sequence'
+ * The numbers should be separated by comma, followed by a space ,
+ * You are allowed to use the standard library
+ * You are not allowed to use any other library (You can’t use GMP etc…)
+ * You are not allowed to use long long, malloc, pointers, arrays/tables, or structures
+ * You are not allowed to hard code any Fibonacci number (except for 1 and 2)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ * The program does not use printf or put function.
+ */
+int main(void)
+{
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
+
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
+	{
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
