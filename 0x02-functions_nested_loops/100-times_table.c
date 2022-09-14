189162173b51a39d2df8df0fa5a331856030b0b4
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-14 02:49:05 -0700

    Print table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,55 +6,49 @@
 /*Betty Lint Style*/
 
 /**
- * print_times_table - Entry point.
- * @n: The character to be checked.
- * print_times_table - print times table.
- * _putchar - writes the character c to stdout.
- * Description: 'print_times_table'
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- * The program does not use printf or put function.
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
  */
 void print_times_table(int n)
 {
-	int x, y, z;
+	int x = 0, y, z;
 
-	if (n >= 0 && n <= 14)
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		for (x = 0; x <= n; x++)
+		for (y = 0; y <= n; y++)
 		{
-			for (y = 0; y <= n; y++)
+			z = x * y;
+			if (z > 99)
 			{
-				z = x * y;
-				if (z > 99)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else if (z > 9)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-				else
-				{
-					if (y != 0)
-					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar(z + '0');
-				}
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
 			}
-			_putchar('\n');
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
+		x++;
 	}
 }
