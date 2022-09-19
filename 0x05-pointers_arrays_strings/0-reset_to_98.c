5ec9e8721e490b7977e7ea64a47d50dca8e1151b
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-19 08:03:40 -0700

    Initial Commit : a function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*Authored by Moses Morris*/
+/*Function file*/
+/*Betty Lint Coding and format style*/
+
+/**
+ * reset_to_98 -  a function that checks for a digit 0-9
+ * @n: input
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
