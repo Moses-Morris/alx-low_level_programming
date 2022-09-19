10ffa38bcdf8480f091090636760337547827128
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-19 13:49:59 -0700

    Fix 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,11 +13,12 @@ void rev_string(char *s)
 {
 	int i, len, temp;
 
-	len = strlen(str1);
+	len = strlen(s);
 	for (i = 0; i < len / 2; i++)
 	{
-		temp = str1[i];
-		str1[i] = str1[len - i - 1];
-		str1[len - i - 1] = temp;
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
+	return (s);
 }
