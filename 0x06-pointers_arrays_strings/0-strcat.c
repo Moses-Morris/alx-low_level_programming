f0be8195c7f3a1108b3b1fdf0aa6ecf0b8a6deee
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-22 01:31:05 -0700

    Initial First commit: Readme file and new directory.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/*Authored by Moses Morris*/
+/*Function file*/
+/*Betty Lint Coding and format style*/
+
+/**
+ * _strcat - a function that concatenates 2 strings.
+ *
+ * @dest: First declared char type : Initialized.
+ *
+ * @src: Second declared char type : Initialized.
+ *
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+
