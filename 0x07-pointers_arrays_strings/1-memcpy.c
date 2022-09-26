e2b685e6de9886ef512b9deebe66d6ba504b8ad9
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-26 11:29:15 -0700

    A function that copies memory area.

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*Authored by Moses Morris*/
+/*Function file*/
+/*Betty Lint Coding and format style*/
+
+/**
+ * _memcpy - A function that copies memory area...
+ *
+ * @dest: Declare variable of Char type.
+ *
+ * @src: Declare variable of Char type.
+ *
+ * @n: Declare variable of unsigned int type.
+ *
+ * Return: 0 on no return and 1 on success and -1 on error(stderr)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int increament;
+
+	for (increament = 0; increament < n; increament++)
+		dest[increament] = src[increament];
+
+	return (dest);
+}
