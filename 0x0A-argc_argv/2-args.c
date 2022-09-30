e96cce385af6bced0b37df2eaf1101325d80a12a
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-30 12:04:53 -0700

    A program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*Authored by Moses Morris*/
+/*Function file*/
+/*Betty Lint Coding and format style*/
+
+/**
+ * main - Entry point : Prints all arguments it receives...
+ *
+ * @argv: Declare an array variable of Char type.
+ *
+ * @argc: Declare a variable of int type.
+ *
+ * Return: 0 on no return and 1 on success and -1 on error(stderr)
+ */
+int main(int argc, char *argv[])
+{
+	int count_args;
+
+	for (count_args = 0; count_args < argc; count_args++)
+	{
+		printf("%s\n", argv[count_args]);
+	}
+
+	return (0);
+}
