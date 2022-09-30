1244b51eafe669d883a3c3e61678c308a0822553
Moses-Morris <mosesmorrisdev@gmail.com>
2022-09-30 12:08:46 -0700

    A  program that multiplies two numbers.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/*Authored by Moses Morris*/
+/*Function file*/
+/*Betty Lint Coding and format style*/
+
+/**
+ * main - Entry point :  Program that multiplies two numbers...
+ *
+ * @argv: Declare an array variable of Char type.
+ *
+ * @argc: Declare a variable of int type.
+ *
+ * Return: 0 on no return and 1 on success and -1 on error(stderr)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
