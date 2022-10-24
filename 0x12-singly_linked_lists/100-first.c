2e1ba42abc5bc7737880d113799b15fe3dddbd67
Moses-Morris <mosesmorrisdev@gmail.com>
2022-10-24 11:39:03 -0700

    A function that executes before main->

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * beforeMain - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) beforeMain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
